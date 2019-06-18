11989ac2d7a234b021f5a9c30fb8fade2618cc7d
Daniil Postnov <dpostnov@ronasit.com>
2019-06-18 19:51:59 +0300

    put the code in order

--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -1,20 +1,16 @@
 console.log('i am work!', 'Change this message in "src/js/partails/common.js"');
 
-
 // This template support es6 syntax
 
 // let, destructurization
-let arr = ['Postnov', 'Daniil'];
+let arr = ['Postnov', 'Daniil'],
+  [firstName, lastName] = arr,
+  func = (message) => console.log(message);
 
-let [firstName, lastName] = arr;
+
 console.log('Author this template: ', firstName, lastName);
 
 // const, template string
 const str = `Hello, i am ${firstName} ${lastName} from console and arrow function`;
-
-// arrow functions
-let func = (message) => {
-  console.log(message);
-};
 
 func(str);
