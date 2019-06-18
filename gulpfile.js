866395cadbd31cc0f9f1c532d43e7a582db42c8c
Daniil Postnov <dpostnov@ronasit.com>
2019-06-18 19:31:27 +0300

    remove svg operations

--- gulpfile.js
+++ gulpfile.js
@@ -29,15 +29,7 @@ var gulp        = require('gulp'),
     html2pug    = require('gulp-html2pug'),
 
     //img
-    tinypng     = require('gulp-tinypng-unlimited'),
-
-
-    //svg
-	svgmin = require('gulp-svgmin'),
-	cheerio = require('gulp-cheerio'),
-    replace = require('gulp-replace'),
-    svgSprite = require('gulp-svg-sprite'),
-    html2pug = require('gulp-html2pug');
+    tinypng     = require('gulp-tinypng-unlimited');
 
 
 
@@ -49,7 +41,6 @@ var path = {
         css      : 'dist/css/',
         img      : 'dist/images/',
         fonts    : 'dist/fonts/',
-        svgSprite: 'dist/images/'
     },
     src: {
         pug     : 'src/pug/*.pug',
@@ -59,7 +50,6 @@ var path = {
         jsLint  : 'src/js/partails/common.js',
         img     : 'src/images/**/*.*',
         fonts   : 'src/fonts/**/*.*',
-        svgSep  : 'src/images/svg-separate/**/*.svg',
     },
     watch: {
         js:     'src/**/*.js',
@@ -67,7 +57,6 @@ var path = {
         css:    'src/**/*.scss',
         img:    'src/images/**/*.*',
         fonts:  'src/fonts/**/*.*',
-        svg:    'src/images/**/*.svg'
     },
     clean: './dist',
     cleanImg: './dist/images'
@@ -83,99 +72,39 @@ var config = {
     port: 3001,
 };
 
-var svgconfig = {
-    shape: {
-        dimension: {         // Set maximum dimensions
-            maxWidth: 500,
-            maxHeight: 500
-        },
-        spacing: {         // Add padding
-            padding: 0
-        },
-    },
-    mode: {
-        stack: {
-            sprite: 'sprite.pug'
-        }
-    }
-};
-
-
-gulp.task('svg:compile', function (cb) {
-    return gulp.src(path.src.svgSep)
-        //minify svg
-        .pipe(svgmin({
-            js2svg: {
-                pretty: true
-            }
-        }))
-		// remove all fill and style declarations in out shapes
-		.pipe(cheerio({
-			run: function ($) {
-				$('[fill]').removeAttr('fill');
-                $('[style]').removeAttr('style');
-			},
-			parserOptions: { xmlMode: true }
-        }))
-		// cheerio plugin create unnecessary string '>', so replace it.
-		.pipe(replace('&gt;', '>'))
-        .pipe(svgSprite(svgconfig))
-        .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-        })
-        .pipe(gulp.dest('src/pug/partails'))
-});
-
-gulp.task('svg:cached', function() {
-    return gulp.src('src/pug/partails/stack/sprite-result.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-        })
-        .pipe(gulp.dest(path.dist.svgSprite))
-});
-
-gulp.task('svg:build', gulpSequence(
-    ['svg:compile'], ['svg:cached']
-))
-
 gulp.task('pug:build', function() {
-    return gulp.src(path.src.pug)
-        // .pipe(changed(path.dist.html, {extension: '.html'})) // раскомментируйте эту строку для того, чтобы pug компилировал только корневые файлы в pug/*.pug. Это позволит уменьшить время компиляции
-        .pipe(
-          pug({pretty: true})
-          .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-          })
-        )
-        .pipe(gulp.dest(path.dist.html))
-        .pipe(reload({ stream: true }));
+  return gulp.src(path.src.pug)
+  // .pipe(changed(path.dist.html, {extension: '.html'})) // раскомментируйте эту строку для того, чтобы pug компилировал только корневые файлы в pug/*.pug. Это позволит уменьшить время компиляции
+  .pipe(
+    pug({pretty: true})
+    .on('error', function (err) {
+      console.log(err.toString());
+      this.emit('end');
+    })
+  )
+  .pipe(gulp.dest(path.dist.html))
+  .pipe(reload({ stream: true }));
 })
 
 
 gulp.task('pug:comb', function () {
-    return gulp.src(path.src.pug)
-        .pipe(
-          pug({
-            pretty: true
-          })
-          .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-          })
-        )
-        .pipe(prettyHtml({
-            indent_size: 4,
-            indent_char: ' ',
-            unformatted: ['code', 'pre', 'em', 'strong', 'i', 'b', 'br']
-        }))
-        .pipe(gulp.dest(path.dist.html))
-        .pipe(reload({ stream: true }));
+  return gulp.src(path.src.pug)
+    .pipe(
+      pug({
+        pretty: true
+      })
+      .on('error', function (err) {
+        console.log(err.toString());
+        this.emit('end');
+      })
+    )
+    .pipe(prettyHtml({
+        indent_size: 4,
+        indent_char: ' ',
+        unformatted: ['code', 'pre', 'em', 'strong', 'i', 'b', 'br']
+    }))
+    .pipe(gulp.dest(path.dist.html))
+    .pipe(reload({ stream: true }));
 });
 
 
@@ -286,16 +215,15 @@ gulp.task('fonts:build', function() {
 
 
 gulp.task('build', gulpSequence(
-    ['svg:build','pug:build', 'css:build', 'js:build', 'images:optimize', 'fonts:build'],
+    ['pug:build', 'css:build', 'js:build', 'images:optimize', 'fonts:build'],
     ['pug:comb','css:comb', 'js:comb']
 ));
 
-gulp.task('watch', ['svg:build','pug:build', 'css:build', 'js:build', 'images:build', 'images:build'], function () {
+gulp.task('watch', ['pug:build', 'css:build', 'js:build', 'images:build', 'images:build'], function () {
     gulp.watch(path.watch.css, ['css:build'])
     gulp.watch(path.watch.js, ['js:build', 'js:lint'])
     gulp.watch(path.watch.pug, ['pug:build'])
     gulp.watch(path.watch.img, ['images:build'])
-    gulp.watch(path.watch.svg, ['svg:build'])
 })
 
 gulp.task('webserver', function() {
