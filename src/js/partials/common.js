6d9b32620edf0393733e7fbb9549cb118122cf92
Daniil Postnov <dpostnov@ronasit.com>
2019-09-14 19:51:41 +0300

    delete console output

--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -1,5 +1,3 @@
-console.log('i am work!', 'Change this message in "src/js/partails/common.js"');
-
 // This template support es6 syntax
 
 // let, destructurization
@@ -7,10 +5,5 @@ let arr = ['Postnov', 'Daniil'],
   [firstName, lastName] = arr,
   func = (message) => console.log(message);
 
-
-console.log('Author this template: ', firstName, lastName);
-
 // const, template string
 const str = `Hello, i am ${firstName} ${lastName} from console and arrow function`;
-
-func(str);
